3d9c04c5d76528f045019610b1496005a425af03
sailor-june <102261950+sailor-june@users.noreply.github.com>
2022-10-20 19:32:52 -0400

    Update sparkle_engine_alpha.py

--- sparkle_engine_alpha.py
+++ sparkle_engine_alpha.py
@@ -7,13 +7,17 @@ from tkinter import ttk
 ##defining the grid
 root = Tk()
 frm = ttk.Frame(root, padding=10)
-frm.grid()
+outfrm=ttk.Frame(root, padding=5)
+
+frm.grid(row=1)
+outfrm.grid(row=0, columnspan=3)
+
 frm.columnconfigure(0, weight=0)
-frm.columnconfigure(1, weight=0)
-frm.columnconfigure(2, weight=1)
-frm.rowconfigure(0, weight=1)
-frm.rowconfigure(1,weight=0)
-frm.rowconfigure(2,weight=0)
+frm.columnconfigure(1, weight=1)
+frm.columnconfigure(2, weight=0)
+frm.rowconfigure(0, weight=0)
+frm.rowconfigure(1,weight=1)
+frm.rowconfigure(2,weight=1)
 root.geometry(newGeometry="280x170")
 
 
@@ -44,6 +48,40 @@ def magic():
     ingredients.append(mana)
     ingredients.append(spell)
 
+    if len(ingredients[0])>8:
+        drain=[]
+        for i in ingredients[0]:
+            drain.append(i)
+        drain.remove(drain[0])
+        ingredients[0]=''.join(drain)
+    cast = ''.join(ingredients)
+    print(cast)
+    print('**********')
+    print(ingredients[0])
+    print(len(ingredients[0]))
+    magout.set(cast)
+
+##widgets to populate the grid
+box = tkinter.Message(outfrm, textvariable=magout)
+label_0 = Label(frm, text = "power level:")
+label_1 = Label(frm, textvariable=powerlevel)
+
+##positioning widgets
+
+box.grid(column=0, row=0, columnspan=3)
+label_0.grid(column=1,row=1)
+label_1.grid(column=1,row=2)
+
+
+#buttons go here
+ttk.Button(frm, text="+", command=plus).grid(column=0, row=3)
+ttk.Button(frm, text="-", command=minus).grid(column=2, row=3)
+ttk.Button(frm, text="HARMONIZE").grid(column=1, row=3)
+ttk.Button(frm, text="sparkle", command=magic).grid(column=1,row=4)
+
+## a slider, because why not?
+w = Scale(frm, from_=0, to=200, orient=HORIZONTAL).grid(column=1, row=5)
+root.mainloop()
     if len(ingredients[0])>8:
         drain=[]
         for i in ingredients[0]:
